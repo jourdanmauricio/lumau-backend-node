93aa2a9d6710f2693432313ae58c9b81cf3ecbc6
mauricio <jourdanmauricio@gmail.com>
2023-09-25 13:35:18 -0300

    .

--- config/mailer.js
+++ config/mailer.js
@@ -5,6 +5,11 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: config.emailPort,
   secure: config.emailSecure, // true for 465, false for other ports
+  tls: {
+    maxVersion: 'TLSv1.3',
+    minVersion: 'TLSv1.2',
+    ciphers: 'TLS_AES_128_GCM_SHA256',
+  },
   auth: {
     user: config.emailSend, // generated ethereal user
     pass: config.emailSendPass, // generated ethereal password
